75e08a3d905ac129523b61abffe2ef10b07621bc
marc desideri <marc@mbp-di-marc-2.home>
2023-03-02 22:57:37 +0100

    work on search component still dosen't work

---
+++ src/pages/auth/Search/Search.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import FormSearch from './FormSearch';
+import "./Search.css"
+
+
+function Search() {
+    
+    return (
+        <div>
+            <div className="search-background" ></div>
+            <FormSearch/>
+        </div>
+    );
+}
+
+export default Search;
