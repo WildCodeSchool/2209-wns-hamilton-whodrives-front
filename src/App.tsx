13c9df4bad119d032402246ccf8431b1e85f9525
ValentinLagues <valentin.lagues@gmail.com>
2023-03-31 14:21:33 +0200

    app file changed to test workflow

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> Alex le bosss ! .
+          Edit <code>src/App.tsx</code> C'est écrit dans le coran.
         </p>
         <a
           className="App-link"
