de6e875fcbb02b36e9cb27630cee52e6c8f72a98
ValentinLagues <valentin.lagues@gmail.com>
2023-03-31 14:42:23 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> C'est écrit dans le coran.
+          Edit <code>src/App.tsx</code>
         </p>
         <a
           className="App-link"
