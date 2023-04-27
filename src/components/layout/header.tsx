3c7f6e24a42fac3fcc3e3feadd9058c40868a06d
ValentinLagues <valentin.lagues@gmail.com>
2023-04-27 12:28:28 +0200

    header links added and authpage created

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,22 +1,35 @@
+import "../../global.css";
+import { useNavigate } from "react-router-dom";
+
 export default function HeaderComponent(): JSX.Element {
+  const navigate = useNavigate();
+
   return (
     <div className="w-screen h-20 bg-layoutBlue">
-      <div className="flex flex-row justify-end gap-20 text-white">
-        <div className="flex flex-row">
-          <img
-            className="fill-white"
-            src="/assets/icons/search.png"
-            alt="search icon"
-          />
+      <div className="flex flex-row items-center justify-end h-full gap-20 header-text">
+        <div
+          className="flex flex-row cursor-pointer"
+          onClick={() => {
+            navigate("search-trip");
+          }}
+        >
+          <img className="" src="/assets/icons/search.svg" alt="search icon" />
           <p>Rechercher</p>
         </div>
-        <div className="flex flex-row">
-          <img src="/assets/icons/car.png" alt="car icon" />
+
+        <div
+          className="flex flex-row cursor-pointer"
+          onClick={() => {
+            navigate("create-trip");
+          }}
+        >
+          <img src="/assets/icons/car.svg" alt="car icon" />
           <p>Publier un trajet</p>
         </div>
-        <div className="flex flex-row">
-          <img src="/assets/icons/user.png" alt="user icon" />
-          <img src="/assets/icons/chevron-down.png" alt="chevron down icon" />
+
+        <div className="flex flex-row cursor-pointer">
+          <img src="/assets/icons/user.svg" alt="user icon" />
+          <img src="/assets/icons/chevron-down.svg" alt="chevron down icon" />
         </div>
       </div>
     </div>
