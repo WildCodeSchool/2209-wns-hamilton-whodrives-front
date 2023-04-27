f5afbd5a43d64f3f7507fa8e3b03a2db9cc4c2d0
ValentinLagues <valentin.lagues@gmail.com>
2023-04-27 12:28:03 +0200

    router config done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import HeaderComponent from "./components/layout/header";
+import AuthPage from "./pages/auth/AuthPage";
+import CreateTripPage from "./pages/createTrip/createTripPage";
+import HomePage from "./pages/home/homePage";
+import ProfilePage from "./pages/profile/profilePage";
+import SearchTripPage from "./pages/searchTrip/searchTripPage";
 
 function App() {
   return (
-    <div className="w-screen h-screen bg-whodrivesGreen">
+    <div className="w-screen h-screen">
       <HeaderComponent />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="profile" element={<ProfilePage />} />
+        <Route path="create-trip" element={<CreateTripPage />} />
+        <Route path="search-trip" element={<SearchTripPage />} />
+        <Route path="auth" element={<AuthPage />} />
+      </Routes>
     </div>
   );
 }
