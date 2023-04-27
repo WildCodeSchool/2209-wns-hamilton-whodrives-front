2a27b1ad2f65dc733eb050d5566277bb939f4dc9
ValentinLagues <valentin.lagues@gmail.com>
2023-04-27 16:06:20 +0200

    assets icons modified and header changed

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,4 +1,4 @@
-import "../../global.css";
+import "../../styles/layout.css";
 import { useNavigate } from "react-router-dom";
 
 export default function HeaderComponent(): JSX.Element {
@@ -8,28 +8,44 @@ export default function HeaderComponent(): JSX.Element {
     <div className="w-screen h-20 bg-layoutBlue">
       <div className="flex flex-row items-center justify-end h-full gap-20 header-text">
         <div
-          className="flex flex-row cursor-pointer"
+          className="flex flex-row gap-2 cursor-pointer"
           onClick={() => {
             navigate("search-trip");
           }}
         >
-          <img className="" src="/assets/icons/search.svg" alt="search icon" />
+          <img
+            className="w-6"
+            src="/assets/icons/search-white.svg"
+            alt="search icon"
+          />
           <p>Rechercher</p>
         </div>
 
         <div
-          className="flex flex-row cursor-pointer"
+          className="flex flex-row gap-2 cursor-pointer"
           onClick={() => {
             navigate("create-trip");
           }}
         >
-          <img src="/assets/icons/car.svg" alt="car icon" />
+          <img
+            className="w-6"
+            src="/assets/icons/car-white.svg"
+            alt="car icon"
+          />
           <p>Publier un trajet</p>
         </div>
 
         <div className="flex flex-row cursor-pointer">
-          <img src="/assets/icons/user.svg" alt="user icon" />
-          <img src="/assets/icons/chevron-down.svg" alt="chevron down icon" />
+          <img
+            className="w-10"
+            src="/assets/icons/user-white.svg"
+            alt="user icon"
+          />
+          <img
+            className="w-8"
+            src="/assets/icons/chevron-down-white.svg"
+            alt="chevron down icon"
+          />
         </div>
       </div>
     </div>
