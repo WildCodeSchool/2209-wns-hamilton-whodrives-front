6f2ee1f78d83dd5dd8311eb5ebfde4120bf1f597
Mazoyer <mazoyeralexandre@hotmail.fr>
2023-04-27 16:13:54 +0200

    add page creatTrip

--- src/pages/createTrip/createTripPage.tsx
+++ src/pages/createTrip/createTripPage.tsx
@@ -1,5 +1,4 @@
 import LocationField from "../../components/createTrip/LocationFields";
-import ReturnTrip from "../../components/createTrip/ReturnTrip";
 
 export default function CreateTripPage(): JSX.Element {
   return <div><LocationField/></div>;
