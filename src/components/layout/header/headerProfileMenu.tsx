28e4563e457c0240bb0b1d17b4f4d2b2f1a09baf
ValentinLagues <valentin.lagues@gmail.com>
2023-04-28 11:59:44 +0200

    header responsive

--- src/components/layout/header/headerProfileMenu.tsx
+++ src/components/layout/header/headerProfileMenu.tsx
@@ -16,6 +16,15 @@ const menuItems: MenuItems[] = [
   { path: "/profile/logout", name: "Déconnexion" },
 ];
 
+const headerLinks: MenuItems[] = [
+  { path: "/search-trip", name: "Rechercher" },
+  { path: "/create-trip", name: "Publier un trajet" },
+];
+
+const responsiveMenuItems: MenuItems[] = headerLinks
+  .concat(menuItems)
+  .map((obj) => ({ ...obj }));
+
 export default function HeaderProfileMenu(): JSX.Element {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
@@ -33,50 +42,95 @@ export default function HeaderProfileMenu(): JSX.Element {
 
   return (
     <div>
-      <button
-        className="flex flex-row"
-        aria-controls={open ? "basic-menu" : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-        onClick={handleClick}
-      >
-        <img
-          className="w-10"
-          src="/assets/icons/user-white.svg"
-          alt="user icon"
-        />
-        <img
-          className="w-8"
-          src={
-            isOpen
-              ? "/assets/icons/chevron-up-white.svg"
-              : "/assets/icons/chevron-down-white.svg"
-          }
-          alt="chevron down icon"
-        />
-      </button>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          "aria-labelledby": "basic-button",
-        }}
-      >
-        {menuItems.map((item: MenuItems, index: number) => {
-          return (
-            <MenuItem key={index} onClick={handleClose}>
-              <a
-                className="header-profile-text hover:text-validBlue"
-                href={item.path}
-              >
-                {item.name}
-              </a>
-            </MenuItem>
-          );
-        })}
-      </Menu>
+      <div className="hidden sm:block">
+        <button
+          className="flex flex-row"
+          aria-controls={open ? "basic-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+        >
+          <img
+            className="w-10"
+            src="/assets/icons/user-white.svg"
+            alt="user icon"
+          />
+          <img
+            className="w-8"
+            src={
+              isOpen
+                ? "/assets/icons/chevron-up-white.svg"
+                : "/assets/icons/chevron-down-white.svg"
+            }
+            alt="chevron down icon"
+          />
+        </button>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          {menuItems.map((item: MenuItems, index: number) => {
+            return (
+              <MenuItem key={index} onClick={handleClose}>
+                <a
+                  className="header-profile-text hover:text-validBlue"
+                  href={item.path}
+                >
+                  {item.name}
+                </a>
+              </MenuItem>
+            );
+          })}
+        </Menu>
+      </div>
+
+      <div className="flex flex-row justify-end h-full sm:hidden">
+        <button
+          className="flex flex-row"
+          aria-controls={open ? "basic-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+        >
+          <img
+            className="w-8"
+            src={
+              isOpen
+                ? "/assets/icons/chevron-up-white.svg"
+                : "/assets/icons/chevron-down-white.svg"
+            }
+            alt="chevron down icon"
+          />
+        </button>
+        <Menu
+          className="sm:hidden"
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          {responsiveMenuItems.map((item: MenuItems, index: number) => {
+            return (
+              <MenuItem key={index} onClick={handleClose}>
+                <a
+                  className="header-profile-text hover:text-validBlue"
+                  href={item.path}
+                >
+                  {item.name}
+                </a>
+              </MenuItem>
+            );
+          })}
+        </Menu>
+      </div>
     </div>
   );
 }
