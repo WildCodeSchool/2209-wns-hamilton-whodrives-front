ecff0aa8cf7c0707bccf5bc883efac3bcb6789be
Mazoyer <mazoyeralexandre@hotmail.fr>
2023-04-28 12:22:34 +0200

    add update

--- src/pages/createTrip/createTripPage.tsx
+++ src/pages/createTrip/createTripPage.tsx
@@ -1,4 +1,5 @@
 import LocationField from "../../components/createTrip/LocationFields";
+import { Route, Routes } from "react-router-dom";
 
 // recuperer les donner du formulaire et les envoyer a la page suivante
 
