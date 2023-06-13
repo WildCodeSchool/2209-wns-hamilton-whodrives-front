7f7893c81c28629f40b670bcb269178b4f2ec537
marc desideri <marc@MacBook-Pro-di-marc-2.local>
2023-06-13 10:32:14 +0200

    formating css done for result card search trip

--- src/components/searchTrip/CardSearchTripResult.tsx
+++ src/components/searchTrip/CardSearchTripResult.tsx
@@ -10,7 +10,7 @@ interface PropsResult {
 function CardSearchTripResult({date,départ,arrivé,tarif,place,submitTrip}:PropsResult) {
   return (
 <>
-<div className="flex flex-col justify-between">
+<div className="w-1/1 flex flex-col pl-2 pr-2 justify-between  ">
         <p>{date.toDateString()}</p>
         <div className="flex flex-row">
           <p>{départ}</p>
@@ -19,9 +19,9 @@ function CardSearchTripResult({date,départ,arrivé,tarif,place,submitTrip}:Prop
         </div>
         <p>Prix du trajet : {tarif}€</p>
       </div>
-      <div >
-        <p className="text-#1195C3">Place Disponibles</p>
-        <p>{place}</p>
+     
+      <div className="flex flex-col w-1/1" >
+        <p className="text-#1195C3">Place Disponibles : {place}</p>
         <button onClick={submitTrip}>j'y vais</button>
       </div>
 </>
