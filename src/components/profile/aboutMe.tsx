7905f5721334693f4ce22d749e04dd43f7bababd
abdallah aissiou <aissiou.abdallah@gmail.com>
2023-06-22 15:12:21 +0200

    fix context

--- src/components/profile/aboutMe.tsx
+++ src/components/profile/aboutMe.tsx
@@ -72,49 +72,58 @@ const AboutMeComponent = () => {
   const user = data.userLogged;
 
   return (
-    <div className=" flex border-2 border-blue-500 p-8 m-20">
-        <div className="w-1/3 mr-5" >
-          <img src="/assets/images/dot-megachx.jpg" alt="" className="w-64"/>
-          <p>{user.username}</p>
-        </div>
-        <div className="w-2/3">
-          <h3 className="bg-blue-700 text-white">A propos de moi</h3>
-          <p>Salut, je m’appelle<span className="text-validBlue"> {user.lastname}</span>, j’ai <span className="text-validBlue">{user.userInfo.age} </span> ans et j’habite à <span className="text-validBlue">{user.userInfo.city}</span> .</p>
+    <div className="flex border-2 border-blue-500 p-8 m-20">
+      <div className="w-1/3 mr-5">
+        <img src="/assets/images/dot-megachx.jpg" alt="" className="w-64" />
+        <p>{user.username}</p>
+      </div>
+      <div className="w-2/3">
+        <h3 className="bg-blue-700 text-white">A propos de moi</h3>
+        {user.userInfo ? (
+          <p>
+            Salut, je m’appelle<span className="text-validBlue"> {user.lastname}</span> j'ai {user.userInfo.age ? <span className="text-validBlue">{user.userInfo.age} ans</span> : null} et j’habite à
+            <span className="text-validBlue"> {user.userInfo.city}</span>.
+          </p>
+        ) : null}
+        {user.userInfo && (
           <div className="Description mt-2">
             <h4>Description</h4>
             <p className="ml-3">{user.userInfo.about.description}</p>
           </div>
-          <h4>Préférences</h4>
-            <div className="flex">
-              <div className="flex mr-5">
-                <img src="/assets/icons/chat-black.svg" alt="" /><p>{user.userInfo.about.chatOption.content}</p>
-              </div>
-              <div className="flex">
-                <img src="/assets/icons/music-black.svg" alt="" /><p>{user.userInfo.about.musicOption.content}</p>
-              </div>
+        )}
+        <h4>Préférences</h4>
+        {user.userInfo && (
+          <div className="flex">
+            <div className="flex mr-5">
+              <img src="/assets/icons/chat-black.svg" alt="" />
+              <p>{user.userInfo.about.chatOption.content}</p>
             </div>
             <div className="flex">
-              <div>
-                 {user.userInfo.about.animal === true ? <img src="/assets/icons/animal-blue.svg" alt="" /> : <img src="/assets/icons/animal-grey.svg" alt="" />}
-              </div>
-              <div className="ml-5">
-                {user.userInfo.about.smoke === true ? <img src="/assets/icons/wind-blue.svg" alt="" /> : <img src="/assets/icons/wind-grey.svg" alt="" />}
-              </div>
-              
+              <img src="/assets/icons/music-black.svg" alt="" />
+              <p>{user.userInfo.about.musicOption.content}</p>
             </div>
-            
-          <h3 className="bg-blue-700 text-white">Ma Voiture</h3>
-          {user.cars.map((car: any) => (
-              <div className="Cars flex" key={car.id}>
-                <img src="/assets/images/yellow-car.png" alt="" className="w-64"/>
-                <p>Ma voiture est une <span className="text-validBlue">{car.model.name}</span>  qui possède <span className="text-validBlue">{car.seat}</span> places.</p>
-              </div>
-            ))}
-           
-          
-        </div>
-        
-     
+          </div>
+        )}
+        {user.userInfo && (
+          <div className="flex">
+            <div>{user.userInfo.about.animal ? <img src="/assets/icons/animal-blue.svg" alt="" /> : <img src="/assets/icons/animal-grey.svg" alt="" />}</div>
+            <div className="ml-5">
+              {user.userInfo.about.smoke ? <img src="/assets/icons/wind-blue.svg" alt="" /> : <img src="/assets/icons/wind-grey.svg" alt="" />}
+            </div>
+          </div>
+        )}
+
+        <h3 className="bg-blue-700 text-white">Ma Voiture</h3>
+        {user.cars.map((car: any) => (
+          <div className="Cars flex" key={car.id}>
+            <img src="/assets/images/yellow-car.png" alt="" className="w-64" />
+            <p>
+              Ma voiture est une <span className="text-validBlue">{car.model.name}</span> qui possède{' '}
+              <span className="text-validBlue">{car.seat}</span> places.
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
