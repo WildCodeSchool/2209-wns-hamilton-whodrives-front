2cedb3a434cacf65e6b743869652c89a3ad90512
ValentinLagues <valentin.lagues@gmail.com>
2023-07-21 01:09:48 +0200

    profile car page changed and dashboard desktop css done

--- src/components/dashboard/DashboardCard.tsx
+++ src/components/dashboard/DashboardCard.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect } from "react";
 import { useQuery, useMutation, gql } from "@apollo/client";
+import moment from "moment";
 
-interface Trip {
+interface ITrip {
   id: string;
   departure_places: string;
   destination: string;
   date_departure: string;
   arrival_date: string;
   hour_departure: string;
+  price: string;
   users: {
     username: string;
   }[];
@@ -16,8 +18,8 @@ interface Trip {
   }[];
 }
 
-interface UserTripsData {
-  UserTripsLoggedUser: Trip[];
+interface IUserTripsData {
+  UserTripsLoggedUser: ITrip[];
 }
 
 const GET_USER_TRIPS = gql`
@@ -29,6 +31,7 @@ const GET_USER_TRIPS = gql`
       date_departure
       arrival_date
       hour_departure
+      price
       users {
         username
       }
@@ -48,7 +51,7 @@ const DELETE_TRIP = gql`
   }
 `;
 
-const GET_USER_LOGGED = gql`
+const GET_USER_LOGGED_USERNAME = gql`
   query getUserLogged {
     userLogged {
       username
@@ -56,25 +59,18 @@ const GET_USER_LOGGED = gql`
   }
 `;
 
-export default function DashboardCard(): JSX.Element {
-  const token = localStorage.getItem("token");
-
+export default function DashboardCard() {
   const {
     loading: tripsLoading,
     error: tripsError,
     data: tripsData,
-  } = useQuery<UserTripsData>(GET_USER_TRIPS, {
-    context: {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    },
-  });
+    refetch,
+  } = useQuery<IUserTripsData>(GET_USER_TRIPS);
   const {
     loading: userLoading,
     error: userError,
     data: userData,
-  } = useQuery(GET_USER_LOGGED);
+  } = useQuery(GET_USER_LOGGED_USERNAME);
 
   const [deleteTripMutation] = useMutation(DELETE_TRIP);
 
@@ -92,9 +88,15 @@ export default function DashboardCard(): JSX.Element {
       );
     }
   }, [tripsError, userError]);
-  if (tripsLoading || userLoading) {
-    return <p>Chargement en cours...</p>;
-  }
+
+  useEffect(() => {
+    refetch();
+  }, []);
+
+  //   if (tripsLoading || userLoading) {
+  //     return <p>Chargement en cours de chargement...</p>;
+  //   }
+
   if (tripsError || userError) {
     return (
       <p>
@@ -111,7 +113,7 @@ export default function DashboardCard(): JSX.Element {
     deleteTripMutation({
       variables: { deleteTripId: tripId },
       update: (cache) => {
-        const existingTrips = cache.readQuery<UserTripsData>({
+        const existingTrips = cache.readQuery<IUserTripsData>({
           query: GET_USER_TRIPS,
         });
 
@@ -120,7 +122,7 @@ export default function DashboardCard(): JSX.Element {
             (trip) => trip.id !== tripId
           );
 
-          cache.writeQuery<UserTripsData>({
+          cache.writeQuery<IUserTripsData>({
             query: GET_USER_TRIPS,
             data: { UserTripsLoggedUser: updatedTrips },
           });
@@ -135,59 +137,79 @@ export default function DashboardCard(): JSX.Element {
         console.error("An error occurred during trip deletion:", error);
       });
   };
-
   return (
-    <div className="flex flex-col flex-grow">
-      {trips.map((trip: Trip) => (
-        <div
-          key={trip.id}
-          className={`border border-gray-300 rounded p-4 flex flex-wrap justify-center m-3 ${
-            trip.users.find((user) => user.username === userLogged.username)
-              ? "bg-green-400 text-white"
-              : trip.passengers.find(
-                  (passenger) => passenger.username === userLogged.username
-                )
-              ? "bg-red-400 text-white"
-              : ""
-          }`}
-        >
-          <div className="flex items-center">
-            <p className="m-3 font-bold">{trip.date_departure}</p>
-            <p className="m-3 font-bold">{trip.hour_departure.split(":00")}</p>
-            <p className="m-3 font-bold">{trip.departure_places}</p>
-            <img src="assets/icons/arrow-right-white.svg" alt="" />
+    <div className="flex flex-col w-5/6 p-8 m-auto my-4 border-2 md:flex-row md:w-1/2 border-validBlue">
+      <div className="flex flex-col flex-grow">
+        {trips.map((trip: ITrip) => (
+          <div
+            key={trip.id}
+            className={`border border-black p-8 flex justify-center m-3 ${
+              trip.users.find((user) => user.username === userLogged.username)
+                ? "bg-whodrivesGreen text-white"
+                : trip.passengers.find(
+                    (passenger) => passenger.username === userLogged.username
+                  )
+                ? "bg-whodrivesPink text-white"
+                : ""
+            }`}
+          >
+            <div className="flex flex-col justify-center w-1/3 gap-4 font-bold">
+              <div className="flex flex-col font-bold">
+                <p>Conducteur :</p>
+                {trip.users.map((user) => (
+                  <p key={user.username} className="ml-1">
+                    {user.username}
+                  </p>
+                ))}
+              </div>
+              {trip.users.find(
+                (user) => user.username === userLogged.username
+              ) ? (
+                <div className="flex flex-col font-bold">
+                  <p>Passagers :</p>
+                  {trip.passengers.map((passenger) => (
+                    <p key={passenger.username} className="ml-1 mr-2">
+                      {passenger.username}
+                    </p>
+                  ))}
+                </div>
+              ) : null}
+            </div>
+            <div className="flex flex-col justify-center w-2/3 gap-3 font-bold">
+              <div className="flex">
+                <p className="mr-2">{trip.departure_places}</p>
+                <img src="assets/icons/arrow-right-white.svg" alt="" />
+                <p className="ml-2">{trip.destination}</p>
+              </div>
+              <p>
+                {moment(trip.date_departure).format("DD/MM/YYYY")} -{" "}
+                {trip.hour_departure.split(":00")}
+              </p>
+              <p>Prix : {trip.price} €</p>
+            </div>
+            <div className="w-1/5">
+              <button
+                type="submit"
+                className="p-4"
+                onClick={() => handleDeleteTrip(trip.id)}
+              >
+                <p className="p-2 text-xs red-button">
+                  {trip.users.find(
+                    (user) => user.username === userLogged.username
+                  )
+                    ? "Supprimer ce trajet"
+                    : trip.passengers.find(
+                        (passenger) =>
+                          passenger.username === userLogged.username
+                      )
+                    ? "Quitter ce trajet"
+                    : ""}
+                </p>
+              </button>
+            </div>
           </div>
-          <p className="m-3 font-bold text-center">{trip.destination}</p>
-          <div className="flex flex-wrap justify-end">
-            {trip.users
-              .filter((user) => user.username === userLogged.username)
-              .map((user) => (
-                <p key={user.username} className="m-3 mr-2 font-bold">
-                  {user.username}
-                </p>
-              ))}
-            {trip.passengers
-              .filter((passenger) => passenger.username === userLogged.username)
-              .map((passenger) => (
-                <p key={passenger.username} className="m-3 mr-2 font-bold">
-                  {/* {passenger.username} */}
-                </p>
-              ))}
-          </div>
-
-          {trip.users.find((user) => user.username === userLogged.username) ? (
-            <p className="flex m-3 font-bold">
-              Passagers:
-              {trip.passengers.map((passenger) => (
-                <p key={passenger.username} className="ml-2 mr-2 font-bold ">
-                  {passenger.username}
-                </p>
-              ))}
-            </p>
-          ) : null}
-          <button onClick={() => handleDeleteTrip(trip.id)}>X</button>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
